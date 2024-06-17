153e78571308147130c1e5bb508a309f400ad4e5
alkachhirang <alkachhirang3@gmail.com>
2024-06-17 16:00:43 +0530

    heading-change

--- src/components/common/CommonHeading.jsx
+++ src/components/common/CommonHeading.jsx
@@ -4,7 +4,7 @@ export const CommonHeading = ({ heading, className }) => {
   return (
 
     <h2
-      className={`${className} capitalize font-semibold font-montserrat text-6xxl leading-md text-darkblue`}
+      className={`${className} capitalize font-semibold font-montserrat text-3xl sm:text-4xl md:text-5xl lg:text-6xxl leading-md text-darkblue`}
     >
       {heading}
     </h2 >
