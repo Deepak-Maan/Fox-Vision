c7a2242cda6b3980ae1286dc4cd18327de66c6ab
naman-398 <you@example.com>
2024-06-17 10:00:51 +0530

    start whatwedo

---
+++ src/components/fox-vision/WhatWeDo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WhatWeDo = () => {
+    return (
+        <div className='max-w-[1164px] mx-auto px-3 pt-60'>
+            <p className='font-poppins font-bold text-xl leading-lg'>A RESULTS-DRIVEN AGENCY</p>
+            <h2 className='font-semibold font-montserrat text-6xxl lead'>What we do</h2>
+        </div>
+    )
+}
+
+export default WhatWeDo
