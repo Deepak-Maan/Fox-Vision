356017a2935784e20e411021ebaeb1413ab8e6ad
kulvinder02 <kulvinderjangra02@gmail.com>
2024-06-17 09:34:03 +0530

    root

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        darkblue: "#071838",
+        sky: "#05D5FB",
+        purple: "#5925F2",
+        lightpurple: "#E6DEFF",
+        lightsky: "#E9FCFF",
+        darkpurple: "#4B17E6",
+        offpurple: " #4B17E60A",
+      },
+      fontSize: {
+        " xxsm": "8px",
+        "4xxl": "40px",
+        "6xxl": "64px",
+        "8xxl": "88.41px",
+      },
+      lineHeight: {
+        sm: "107.9%",
+        md: "121%",
+        lg: "183%",
+      },
+    },
   },
   plugins: [],
 };
