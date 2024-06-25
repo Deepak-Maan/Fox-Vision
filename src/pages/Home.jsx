e74c51a8c536066faa194b3dfc2823b65c61682c
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-25 11:11:22 +0530

    modified common header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,19 @@ import HowWeWorks from '../components/fox-vision/HowWeWorks'
 import ProjectStarten from '../components/fox-vision/ProjectStarten'
 import LogoDesign from '../components/fox-vision/LogoDesign'
 import Angebot from '../components/fox-vision/Angebot'
-
-
 const Home = () => {
+
   return (
     <div>
-      <CommonHeader 
-        additionalClass="mt-12 lg:mt-16"/>
-      <WhatWeDo/>
-      <LogoDesign/>
+      <div className="2xl:min-h-screen">
+        <CommonHeader
+          additionalClass="mt-12 lg:mt-16" />
+      </div>
+      <WhatWeDo />
+      <LogoDesign />
       <HowWeWorks />
-      <Angebot/>
-      <ProjectStarten bgColor='bg-darkpurple'/>
+      <Angebot />
+      <ProjectStarten bgColor='bg-darkpurple' />
     </div>
   )
 }
